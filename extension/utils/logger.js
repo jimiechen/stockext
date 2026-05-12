44f22706c0d710553be7c84e6fee834deda66e23
StockExt Developer <dev@stockext.local>
2026-05-12 11:03:01 +0800

    fix: logger is not defined in background.js

--- extension/utils/logger.js
+++ extension/utils/logger.js
@@ -17,6 +17,11 @@ function error(...args) {
   console.error(PREFIX, ...args);
 }
 
+// Service Worker global binding
+if (typeof self !== "undefined") {
+  self.logger = { info, warn, error };
+}
+
 if (typeof module !== "undefined" && module.exports) {
   module.exports = { info, warn, error };
 }
